125ce24cdb5fe337cf4bb383d9135ffca26a0899
Abdiel Cordeiro <abdiel.boaz1@gmail.com>
2020-01-08 15:35:55 -0300

    Dependencias para o cucumber

--- src/sucessoTDD/testeCadastrarSucesso.java
+++ src/sucessoTDD/testeCadastrarSucesso.java
@@ -1,29 +1,39 @@
 package sucessoTDD;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+import utility.Utils;
 
 public class testeCadastrarSucesso {
 
 	private WebDriver driver;
 
 	@Before
-	public void carregar() {
-		driver = new FirefoxDriver();
-		driver.manage().window().maximize();
-		driver.get("http://www.advantageonlineshopping.com/#/");
+	public void carregar() throws Exception {
+		driver = Utils.openBrowser("Firefox", "http://www.advantageonlineshopping.com/#/");
+
+		Thread.sleep(1000);
+		WebElement bntLogar = driver.findElement(By.id("hrefUserIcon"));
+		bntLogar.click();
+		Thread.sleep(3000);
+		WebElement bntCadastrar = driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/a[2]"));
+		bntCadastrar.click();
 	}
 
 	@Test
-	public void cadastrar() throws InterruptedException {
-		Thread.sleep(1000);
-		WebElement bntLogin = driver.findElement(By.id("hrefUserIcon"));
-		bntLogin.click();
-		System.out.println("Chegou aqui");
+	public void cadastrar() {
+
+
+	}
+
+	@After
+	public void encerrar() {
+		//Utils.closeBrowser(driver);
 	}
 
 }
