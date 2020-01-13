32b5754c7038dcf2549c3e8ca27084633d77c0eb
Abdiel Cordeiro <abdiel.boaz1@gmail.com>
2020-01-13 14:23:12 -0300

    Adição da massa de dados

---
+++ src/br/com/rsinet/HUB_TDD/utility/Constant.java
@@ -0,0 +1,15 @@
+package br.com.rsinet.HUB_TDD.utility;
+
+public class Constant {
+
+	public static final String URL = "http://www.advantageonlineshopping.com/#/";
+
+	public static final String Path_TestData = "C://Users//abdiel.cordeiro//Documents//Projetos//eclipse-workspace//projetoTDD//src//br//com//rsinet//HUB_TDD//testData//";
+
+	public static final String File_TestData = "BancoDados.xlsx";
+
+	// public static final String Username = "testuser_1";
+
+	// public static final String Password = "Test@123";
+
+}
